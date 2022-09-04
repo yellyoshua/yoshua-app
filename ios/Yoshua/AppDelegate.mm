82f8d5dae70e6398748eedcb554d48dadec80e7d
yellyoshua <usuario1529yoshua@hotmail.com>
2022-09-04 13:29:47 -0500

    ios change app name

--- ios/Yoshua/AppDelegate.mm
+++ ios/Yoshua/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"todo_app", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Yoshua", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
--- ios/YoshuaTests/YoshuaTests.m
+++ ios/YoshuaTests/YoshuaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface todo_appTests : XCTestCase
+@interface YoshuaTests : XCTestCase
 
 @end
 
-@implementation todo_appTests
+@implementation YoshuaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
